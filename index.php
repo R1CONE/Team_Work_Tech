caab1cf78faa8f9b59b056a1d6f95ad26608dd8b
R1CONE <116386177+R1CONE@users.noreply.github.com>
2023-10-31 10:14:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,34 +2,37 @@
 $servername = "localhost"; // Имя сервера базы данных
 $username = "root"; // Имя пользователя базы данных
 $password = ""; // Пароль пользователя
-$database = "test_bd"; // Имя базы данных
+$database = "try_bd"; // Имя базы данных
 
 // Подключение к базе данных
 $conn = new mysqli($servername, $username, $password, $database);
 
 // Проверка соединения
 if ($conn->connect_error) {
-    die("Eror do połoczenia" . $conn->connect_error);
+    die("Ошибка подключения к базе данных: " . $conn->connect_error);
 } else {
-    echo "Wszyskto w porządku";
+    echo "Подключение к базе данных прошло успешно!";
     
-    // Тут можно выполнять операции с базой данных
-    
-    // Закрываем соединение при необходимости
+    // Выполнение SQL-запроса для выборки данных из таблицы "accounts"
+    $sql = "SELECT id, login, password FROM accounts";
+    $result = $conn->query($sql);
 
+    // Проверка наличия данных и их вывод
+    if ($result->num_rows > 0) {
+        echo "<h2>Содержание таблицы accounts:</h2>";
+        echo "<table>";
+        echo "<tr><th>ID</th><th>Login</th><th>Password</th></tr>";
 
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
-$result = mysqli_query($conn, $sql);
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr><td>" . $row["id"] . "</td><td>" . $row["login"] . "</td><td>" . $row["password"] . "</td></tr>";
+        }
 
-if (mysqli_num_rows($result) > 0) {
-  // output data of each row
-  while($row = mysqli_fetch_assoc($result)) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-  }
-} else {
-  echo "0 results";
-}
-    
+        echo "</table>";
+    } else {
+        echo "В таблице нет данных.";
+    }
+
+    // Закрываем соединение
     $conn->close();
 }
 ?>
