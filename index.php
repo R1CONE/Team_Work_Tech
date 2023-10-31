3b8c218cc8fbda2b04751143ed407b26a39abf84
R1CONE <116386177+R1CONE@users.noreply.github.com>
2023-10-31 09:24:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 $servername = "localhost"; // Имя сервера базы данных
 $username = "root"; // Имя пользователя базы данных
 $password = ""; // Пароль пользователя
-$database = "try_bd"; // Имя базы данных
+$database = "test_bd"; // Имя базы данных
 
 // Подключение к базе данных
 $conn = new mysqli($servername, $username, $password, $database);
 
 // Проверка соединения
 if ($conn->connect_error) {
-    die("Ошибка подключения к базе данных: " . $conn->connect_error);
+    die("Eror do połoczenia" . $conn->connect_error);
 } else {
-    echo "Подключение к базе данных прошло успешно!";
+    echo "Wszyskto w porządku";
     
     // Тут можно выполнять операции с базой данных
     
